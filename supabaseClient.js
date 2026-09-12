e552b88b1d1d039ed6fa094e3ff279e111ce11b6
adesetiawandi <158235639+adesetiawandi@users.noreply.github.com>
2026-09-12 20:43:32 +0700

    Add files via upload

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = https://vydiqphuskqjoksekxeq.supabase.co;
-const SUPABASE_ANON_KEY = sb_publishable_DMkGWQiUgP5v42iuSqVuxA_REgNQm4i;
+const SUPABASE_URL = 'https://vydiqphuskqjoksekxeq.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_DMkGWQiUgP5v42iuSqVuxA_REgNQm4i';
 
 const supabaseClient = supabase.createClient(https://vydiqphuskqjoksekxeq.supabase.co, sb_publishable_DMkGWQiUgP5v42iuSqVuxA_REgNQm4i);
